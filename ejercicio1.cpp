09a7889411b5fc66468869be8bc4c76d39049cc3
Karen Suarez Rico <tu-correo@ejemplo.com>
2026-05-03 21:48:35 -0500

    yes

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
 int main () {
 
+    int age;
+
+    cout << "Ingresa su edad: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "Eres mayor de edad." << endl;
+    } else {
+        cout << "Eres menor de edad." << endl;
+    }
     return 0;
 }
