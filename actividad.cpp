71bed29a0828a32091d62c3f53ac9131128c7b37
Karen Suarez Rico <tu-correo@ejemplo.com>
2026-05-06 08:46:14 -0500

    comit

--- actividad.cpp
+++ actividad.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
+#include <string>
 
-class EquipoFutbol {
-private: 
+class Coche {
+private:
+    //Atributos
+    std::string Fabricante
+    int Cilindraje;
+    int Potencia;
+    std::string Color;
+    double Precio;
 
-std::string name;
+public:
+    //Métodos
+    void Encender () {
+        std::cout << "El coche esta encendido." << std::endl;
+    }
+
+    void Acelerar () {
+        std::cout << "El coche esta en marcha." << std::endl;
+    }
+
+    void frenar () {
+        std::cout << "El coche frenó." << std::endl; 
+    }
+
+    void girar () {
+        std::cout << "El coche giró." << std::endl;
+    }
+
+};
+
+//Funcion principal
+int main () {
+
+    Coche c_Jose;
+    Coche c_Maria;
+
+    c_Jose.encender ();
 
 
+
+
+    return 0;
 }
+
